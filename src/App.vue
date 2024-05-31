b913a3c49c47084f4f42b07912c0acfdbfdadea9
Andrei Eronin <andrei.dev2016@yandex.ru>
2024-05-31 23:50:41 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineComponent } from "vue";
+import { NConfigProvider } from "naive-ui";
+// theme
+// locale & dateLocale
+import { ruRU, dateRuRU } from "naive-ui";
+</script>
+
+<template>
+  <n-config-provider :locale="ruRU" :date-locale="dateRuRU">
+    <RouterView />
+  </n-config-provider>
+</template>
+
+<style></style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
