6ced7f3afa5d7a88f46d541c63d99f3517ff2066
Andrei Eronin <57400388+andreidevs@users.noreply.github.com>
2024-06-14 11:02:46 +0500

    f

--- src/router/client.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    path: "/:id",
-    name: "client_home",
-    component: () => import("@/views/client/Home.vue"),
-  },
-];
--- src/views/client/Home.vue
+++
@@ -1,3 +0,0 @@
-<template>Home Clinet</template>
-<script setup></script>
-<style scoped></style>
